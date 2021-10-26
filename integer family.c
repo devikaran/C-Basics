2b6024af96b2ed32e4080048d28e0e50179ad44f
devikaran <92290945+devikaran@users.noreply.github.com>
2021-10-26 12:20:25 +0530

    Add files via upload

---
+++ integer family.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+  unsigned long long int a,b,c,tot;
+	
+  a=1655755485;
+  b=1234748383;
+  c=3745837473;
+  tot=a+b+c;
+  	
+  printf("value of A %lld",a);                                  
+  printf("value of B %lld",b);
+  printf("value of C %lld",c);
+  printf(" total value %lld",a+b+c);
+	
+
+}
