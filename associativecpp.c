1d4aa802c11672b7666c38b949203382c8b6b6fd
devikaran <92290945+devikaran@users.noreply.github.com>
2021-11-08 12:04:59 +0530

    Add files via upload

---
+++ associativecpp.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int a,b ,c,d,e,f,g,h;
+	a=69;
+	b=94;
+	
+	c=a>b;
+	d=a<b;
+	e=a==b;
+	f=a!=b;
+	g=a<=b;
+	h=a>=b;
+	
+	
+	printf("%d>%d-->%d\n",a,b,c);
+	printf("%d<%d-->%d\n",a,b,d);
+	printf("%d==%d-->%d\n",a,b,e);
+	printf("%d!=%d-->%d\n",a,b,f);
+	printf("%d<=%d-->%d\n",a,b,g);
+	printf("%d>=%d-->%d\n",a,b,h);
+}
