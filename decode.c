1c457bbfbfea037ee5df7c63098920aa57bf3345
devikaran <92290945+devikaran@users.noreply.github.com>
2021-11-15 12:33:42 +0530

    Add files via upload

--- decode.c
+++ decode.c
@@ -1,9 +1,10 @@
+/*converting characters into numbers*/
 #include<stdio.h>
 void main()
 {
-	int a,b,c,d;
+	int a, b, c;
 	
-	printf("enter four number");
-	scanf ("%d%d%d%d",&a,&b,&c,&d);
-	printf(" %c%c%c%c",a+95,b+95,c+95,d+95);
+	printf("enter three characters");
+	scanf ("%c%c%c",&a, &b, &c);
+	printf(" %d %d %d", a-95,b-95,c-95);
 }
